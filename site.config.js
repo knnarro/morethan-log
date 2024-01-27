59543c1daebc79bf3e5c4c062ade62ec0722cde9
knnarro <knnarro@gmail.com>
2024-01-28 01:48:52 +0900

    fix: Contact 수정

--- site.config.js
+++ site.config.js
@@ -6,8 +6,8 @@ const CONFIG = {
     role: "Backend Engineer",
     bio: "생산성에 관심이 많은\n조금 게으른 백엔드 개발자입니다.",
     email: "knnarro@gmail.com",
-    linkedin: "https:/www.linkedin.com/in/나래-강-7aa99a249/",
-    github: "https://github.com/knnarro",
+    linkedin: "나래-강-7aa99a249",
+    github: "knnarro",
     instagram: "",
   },
   projects: [
